0e447b46e7c74feaaedcdd4d936fb3cfed3de4f0
Denis Souza <denyssouza1@hotmail.com>
2022-03-26 14:12:56 -0300

    Code and test alert error for server response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,21 +1,25 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 import ScoopOption from "./ScoopOption";
 import ToopingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        //TODO: handle error response
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToopingOption;
 
